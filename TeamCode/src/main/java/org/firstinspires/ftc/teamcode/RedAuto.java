9832bb472cec3a03b239ef202dbb7f4b3d59dccb
Adarsh Kulkarni <addy2999@gmail.com>
2016-10-28 13:30:55 -0400

    Autonomous with light sensing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto.java
@@ -78,7 +78,7 @@ import ftc.electronvolts.statemachine.StateName;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Autonomous for Red Alliance", group="Robowiz")
+@Autonomous(name="Red Test Auto", group="Robowiz")
 //@Disabled
 public class RedAuto extends LinearOpMode {
 
@@ -98,11 +98,11 @@ public class RedAuto extends LinearOpMode {
     VuforiaLocalizer vuforia;
 
     static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                       (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
+    static final double     DRIVE_SPEED             = 0.8;
     static final double     TURN_SPEED              = 0.5;
 
     @Override
